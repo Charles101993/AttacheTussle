b7d3fa70c191e60d478049b187836e6c0dd142e5
charles101993 <charles101993@tamu.edu>
2016-03-29 21:27:39 -0500

    Create app_c.js

---
+++ app_c.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+ 
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+//app.use('/client',express.static(__dirname + '/client'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+serv.listen(10000);
+console.log("Server started.");
+ 
+ 
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+    console.log('socket connection');
+ 
+    socket.on('input',function(data){
+        console.log(data.client);
+        socket.emit('serverMsg',{
+            msg:data.client,
+            player:data.player
+        });
+    });
+});
+
